137eae16d9414887be1834a2cbaae761aac8fcbf
hamitmizrak <hamitmizrak@gmail.com>
2023-04-01 11:38:57 +0300

    Exception

---
+++ src/main/java/com/hamitmizrak/exception/HamitMizrakException.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak.exception;
+
+//kendi exception
+public class HamitMizrakException extends  RuntimeException{
+    public HamitMizrakException(String message) {
+        super(message);
+    }
+}
