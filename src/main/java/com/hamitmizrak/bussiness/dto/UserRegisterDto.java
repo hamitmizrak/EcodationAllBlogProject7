9cdc8c3e6803b5da4a30515ae69b49ebb9c95620
hamitmizrak <hamitmizrak@gmail.com>
2023-04-01 13:12:40 +0300

    UniqueEmailValidation

--- src/main/java/com/hamitmizrak/bussiness/dto/UserRegisterDto.java
+++ src/main/java/com/hamitmizrak/bussiness/dto/UserRegisterDto.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
 import java.util.Date;
 
 //DTO ==> validation
