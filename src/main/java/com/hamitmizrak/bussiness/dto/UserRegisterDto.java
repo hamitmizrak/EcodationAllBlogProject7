8c9e8ccdb6c1e506d42992fd7451cfaeb158315b
hamitmizrak <hamitmizrak@gmail.com>
2023-04-01 10:35:17 +0300

    UserRegisterDto

--- src/main/java/com/hamitmizrak/bussiness/dto/UserRegisterDto.java
+++ src/main/java/com/hamitmizrak/bussiness/dto/UserRegisterDto.java
@@ -1,9 +1,20 @@
 package com.hamitmizrak.bussiness.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class UserRegisterDto {
 
     private Long id;
+    @NotNull(message = "{register.username.validation.constraints.NotNull.message}")
     private String username;
+
+    @NotNull(message = "{register.email.validation.constraints.NotNull.message}")
+    @Email
+    @Size(max = 200)
     private String email;
+
+    @NotNull(message = "{register.password.validation.constraints.NotNull.message}")
     private String password;
 }
