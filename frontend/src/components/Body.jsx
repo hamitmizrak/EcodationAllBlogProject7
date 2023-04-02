dd821359682f022b74e6b4e723690689d677485a
hamitmizrak <hamitmizrak@gmail.com>
2023-04-02 13:56:22 +0300

    Header Footer

---
+++ frontend/src/components/Body.jsx
@@ -0,0 +1,13 @@
+//rcc => TAB react class component
+import React, { Component } from 'react'
+
+export default class Body extends Component {
+  render() {
+    return (
+      <>
+      <br/>
+      Body
+      </>
+    )
+  }
+}
