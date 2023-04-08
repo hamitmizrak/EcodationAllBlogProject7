382c07cb6068d67fdddc760942d01672138d5f18
hamitmizrak <hamitmizrak@gmail.com>
2023-04-08 10:00:05 +0300

    React Class Template

--- frontend/src/components/register/RegisterList.jsx
+++ frontend/src/components/register/RegisterList.jsx
@@ -1,8 +1,32 @@
-// rcf ==> TAB
-import React from 'react'
+// rcc ==> TAB
+import React, { Component } from 'react'
 
-export default function RegisterList() {
-  return (
-    <div>RegisterList</div>
-  )
-}
+// life cycle 
+// 1-) constructor 
+// 2-) render
+// 3-)CDM
+export default class RegisterList extends Component {
+    constructor(props) {
+        super(props);
+
+        //STATE 
+        this.state={
+
+        }
+
+        //BİND
+    }//end constructor
+
+    //CDM 
+
+    //FUNCTION 
+
+
+    //RENDER
+    render() {
+        return (
+            <div>RegisterList</div>
+        )
+    } //end render
+} //end RegisterList
+
